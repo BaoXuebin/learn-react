bdcebc7005961d64d38c148e02549e9eebe5dda2
BaoXuebin <baoxbin@hotmail.com>
2017-05-11 12:32:47 +0800

    重新整理 master 主分支

--- app/app.jsx
+++ app/app.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Profile from './Profile';
-import Deskmark from './Deskmark';
 
-// function App() {
-//     return (
-//         <div className="container">
-//             <h1>Hello Wordlds!</h1>
-//         </div>
-//     );
-// }
-//
 const app = document.createElement('div');
 document.body.appendChild(app);
 
-// const props = {
-//     name: 'BaoXuebin',
-//     age: 21
-// };
+function Root() {
+    return (
+        <h1>Hello World!</h1>
+    );
+}
 
-// ReactDOM.render(<Profile {...props} />, app);
-ReactDOM.render(<Deskmark />, app);
+ReactDOM.render(<Root />, app);
